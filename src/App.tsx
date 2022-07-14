a875d3e4e0c44889edfbbc9da8f59e1eefc10a15
Edward Jones <ejones@bunnings.com.au>
2022-07-14 17:39:56 +1000

    update start screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import logo from "./diy-gas-swap.png";
 import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
@@ -8,14 +8,44 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Self serve gas swap now available at Bunnings.</p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Link
+          to="/empty"
+          style={{
+            textDecoration: "none",
+            margin: "auto",
+            marginBottom: "20px",
+          }}
         >
-          Learn React
-        </a> */}
+          <>
+            <div
+              onClick={() => {}}
+              style={{
+                borderRadius: "16px",
+                width: "87.7vw",
+                maxWidth: "342px",
+                height: "80px",
+                textAlign: "center",
+                border: "solid",
+                borderColor: "white",
+              }}
+            >
+              <p
+                style={{
+                  margin: "0",
+                  lineHeight: "80px",
+                  fontFamily: "Futura Std",
+                  fontStyle: "normal",
+                  fontWeight: 800,
+                  fontSize: "26px",
+                  letterSpacing: "0.4px",
+                  color: "white",
+                }}
+              >
+                Get Started
+              </p>
+            </div>
+          </>
+        </Link>
       </header>
     </div>
   );
