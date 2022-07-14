d6b7bf9a30cda2491d6415ea334bde491aa2a00c
Edward Jones <ejones@bunnings.com.au>
2022-07-14 17:59:09 +1000

    update favicon

--- src/App.tsx
+++ src/App.tsx
@@ -5,48 +5,39 @@ import { Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Self serve gas swap now available at Bunnings.</p>
-        <Link
-          to="/empty"
-          style={{
-            textDecoration: "none",
-            margin: "auto",
-            marginBottom: "20px",
-          }}
-        >
-          <>
-            <div
-              onClick={() => {}}
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Self serve gas swap now available at Bunnings.</p>
+      <Link to="/empty">
+        <>
+          <div
+            onClick={() => {}}
+            style={{
+              borderRadius: "16px",
+              width: "87.7vw",
+              maxWidth: "342px",
+              height: "80px",
+              textAlign: "center",
+              border: "solid",
+              borderColor: "white",
+            }}
+          >
+            <p
               style={{
-                borderRadius: "16px",
-                width: "87.7vw",
-                maxWidth: "342px",
-                height: "80px",
-                textAlign: "center",
-                border: "solid",
-                borderColor: "white",
+                margin: "0",
+                lineHeight: "80px",
+                fontFamily: "Futura Std",
+                fontStyle: "normal",
+                fontWeight: 800,
+                fontSize: "26px",
+                letterSpacing: "0.4px",
+                color: "white",
               }}
             >
-              <p
-                style={{
-                  margin: "0",
-                  lineHeight: "80px",
-                  fontFamily: "Futura Std",
-                  fontStyle: "normal",
-                  fontWeight: 800,
-                  fontSize: "26px",
-                  letterSpacing: "0.4px",
-                  color: "white",
-                }}
-              >
-                Get Started
-              </p>
-            </div>
-          </>
-        </Link>
-      </header>
+              Get Started
+            </p>
+          </div>
+        </>
+      </Link>
     </div>
   );
 }
